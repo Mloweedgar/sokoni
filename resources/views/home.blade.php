a387c1b6c48ad281bc27c4a910c02d53bdd5cfc0
rajab <rajeyw@gmail.com>
2016-04-19 08:58:54 +0300

    layout with login module

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,15 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    You are logged in!
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+@include('layouts.footer')
 @endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,101 @@
+<!--footer navigation-->
+        <div class="app-footer-nav">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="row">
+                            <div class="col-sm-3">SHOPS</div>
+                            <div class="col-sm-3">INFORMATION</div>
+                            <div class="col-sm-2">CUSTOMER SERVICE</div>
+                            <div class="col-sm-4">STAY CONNECTED</div>
+                        </div>
+                    </div>
+                </div>
+            </div>  
+        </div>
+
+        <!--footer links-->
+
+        <div class="app-footer-links">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <ul class="list-unstyled">
+                                    <li><a href="#">link</a></li>
+                                    <li><a href="#">link</a></li>
+                                    <li><a href="#">link</a></li>
+                                    <li><a href="#">link</a></li>
+                                    <li><a href="#">link</a></li>
+                                    <li><a href="#">link</a></li>
+                                    <li><a href="#">link</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-sm-3">
+                                <ul class="list-unstyled">
+                                    <li><a href="#">link</a></li>
+                                    <li><a href="#">link</a></li>
+                                    <li><a href="#">link</a></li>
+                                    <li><a href="#">link</a></li>
+                                    <li><a href="#">link</a></li>
+                                    <li><a href="#">link</a></li>
+                                    <li><a href="#">link</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-sm-2">
+                                <ul class="list-unstyled">
+                                    <li><a href="#">link</a></li>
+                                    <li><a href="#">link</a></li>
+                                    <li><a href="#">link</a></li>
+                                    <li><a href="#">link</a></li>
+                                    <li><a href="#">link</a></li>
+                                    <li><a href="#">link</a></li>
+                                    <li><a href="#">link</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <ul class="nav nav-pills">
+                                            <li><a href="#"><i class="fa fa-facebook-square fa-lg"></i></a></li>
+                                            <li><a href="#"><i class="fa fa-twitter-square fa-lg"></i></a></li>
+                                            <li><a href="#"><i class="fa fa-instagram fa-lg"></i></a></li>
+                                            <li><a href="#"><i class="fa fa-rss-square fa-lg"></i></a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-sm-12" style="margin-top: 20px;">
+                                        <h4>SUBSCRIBE IN OUR NEWSLETTER</h4>
+                                        <form class="form-inline" role="form">
+                                            <div class="input-group" style="width: 100%;">
+                                                <input type="email" placeholder="Enter email address" class="form-control" />
+                                                <div class="input-group-btn">
+                                                    <button type="submit" class="btn btn-primary cust-btn">SUBSCRIBE</button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--footer-->
+        <div class="app-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-8">
+                        <h3>Sokoni</h3>
+                        <p>You can't afford what you are dying for? Dont give up on yourself! Earn shopping points for unlimited shopping discount. All you need is to tell a friend to tell a friend You can't afford what you are dying for? Dont give up on yourself! Earn shopping points for unlimited shopping discount.</p>
+                    </div>
+                    <div class="col-sm-offset-1 col-sm-3">
+                            <h5 style="padding-top: 20px;"><a href="#">CREDITS</a>&nbsp|&nbsp<a href="#">PRIVACY</a>&nbsp|&nbsp<a href="#">CONTACT</a></h5>
+                            <p>Designed by Mkomwa Rajab</p>
+                            <p>+255658 879 834</p>
+                    </div>
+                </div>
+            </div>
+        </div>
