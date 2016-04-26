1532395ce7fde36f1998b1dfd7d036c45f1b9967
Mloweedgar <mloweedgar0@gmail.com>
2016-04-26 13:04:33 +0300

    I have created a folder for email views

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+<h2>Sokoni project!</h2>
+
+<div>
+    Welcome to {!! $name !!} sokoni site!
+</div>
+
+</body>
+</html>
