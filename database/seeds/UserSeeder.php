9bbe82eec8314422cf7ab7f6a062459d9f95c2e2
rajey <rajeyw@gmail.com>
2016-05-13 06:13:40 +0300

    database test

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -16,18 +16,21 @@ class UserSeeder extends Seeder
         1 => [
           'name' => 'rajab',
           'email' => 'raj@example.com',
+          'avatar' => 'avatar',
           'password' => bcrypt('sokoni'),
         ],
 
         2 => [
           'name' => 'rodrick',
           'email' => 'rodrick@example.com',
+          'avatar' => 'avatar',
           'password' => bcrypt('sokoni'),
         ],
 
         3 => [
           'name' => 'edga',
           'email' => 'edga@example.com',
+          'avatar' => 'avatar',
           'password' => bcrypt('sokoni'),
         ],
 
