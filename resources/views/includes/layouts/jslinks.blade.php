924b6a040b74d4dff81aa03ac32bc122eba9fcbb
rajey <rajeyw@gmail.com>
2016-05-26 07:57:44 +0300

    troubleshooting conflict

--- resources/views/includes/layouts/jslinks.blade.php
+++ resources/views/includes/layouts/jslinks.blade.php
@@ -2,15 +2,11 @@
 <script type="text/javascript" src="{{asset('js/jquery-2.2.1.js')}}"></script>
 <script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('js/app.js')}}"></script>
-<<<<<<< HEAD
 <script type="text/javascript" src="{{asset('js/product.js')}}"></script>
 <script type="text/javascript" src="{{asset('bootstrap-slider/dist/bootstrap-slider.min.js')}}"></script>
 <!--
 <script type="text/javascript" src="{{asset('bootstrap-material-design/dist/js/bootstrap-material.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('bootstrap-material-design/dist/css/ripples.min.js')}}"></script>
 -->
-=======
 <script type="text/javascript" src="{{asset('js/profile.js')}}"></script>
 <script type="text/javascript" src="{{asset('js/wishlist.js')}}"></script>
-
->>>>>>> 0b42f56ec975a7575e5746ee587d0b69167017bd
