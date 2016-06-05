5e8f1868366281e4872d9e2d3eeaa3cc862ac40a
rajey <rajeyw@gmail.com>
2016-06-05 13:35:21 +0300

    Api test

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -3,20 +3,45 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Response;
 
 class Cors
 {
+
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)
-              ->header('Access-Control-Allow-Origin', '*')
-              ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+
+        $headers = [
+            'Access-Control-Allow-Origin'      => $_SERVER['HTTP_ORIGIN'],
+            // CORS doesn't accept Access-Control-Allow-Origin = * for security reasons
+            //'Access-Control-Allow-Origin'    => '*',
+            'Access-Control-Allow-Methods'     => 'POST, OPTIONS',
+            //'Access-Control-Allow-Methods'   => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Max-Age'           => '86400',
+            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With',
+            //'Access-Control-Allow-Headers'   => 'X-Custom-Header, X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-security-token',
+        ];
+
+
+        //Using this you don't need an method for 'OPTIONS' on controller
+        if ($request->isMethod('OPTIONS'))
+            return Response::json('{"method":"OPTIONS"}', 200, $headers);
+
+        // For all other cases
+        $response = $next($request);
+        foreach ($headers as $key => $value)
+            $response->header($key, $value);
+
+        return $response;
+
     }
 }
